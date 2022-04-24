9eddf27280969a350c63ce42645feae79ec36d6f
BiancaPalileo <biclpalileo@gmail.com>
2022-04-24 09:59:51 +0100

    Fighters travel to and stop near their targetBase

--- Assets/Fighter.cs
+++ Assets/Fighter.cs
@@ -8,10 +8,14 @@ public class Fighter : MonoBehaviour
     public GameObject[] bases;
     public GameObject myBase;
     public GameObject targetBase;
+    public Arrive arrive;
+    public Boid boid;
 
     // Start is called before the first frame update
     void Start()
     {
+        arrive = GetComponent<Arrive>();
+        boid = GetComponent<Boid>();
         bases = GameObject.FindGameObjectsWithTag("Base");
 
         myBase = gameObject.transform.parent.gameObject;
@@ -21,11 +25,15 @@ public class Fighter : MonoBehaviour
 
             targetBase = bases[index];
         }
+
+        arrive.targetGameObject = targetBase;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Vector3.Distance(targetBase.transform.position, gameObject.transform.position) < 10) {
+            boid.enabled = false;
+        }
     }
 }
